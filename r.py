eb91f1e15285debe1b06c5509258077f5a727fa7
Prasahnt Kumar <kr.prashant94@gmail.com>
2019-11-16 20:06:35 +0530

    data

--- r.py
+++
@@ -1,9 +0,0 @@
-from os import walk
-import os
-f = []
-i = 1
-for (dirpath, dirnames, filenames) in walk("trainparent"):
-    for dirs in dirnames:
-    	os.rename("trainparent"+os.sep+str(dirs), "trainparent"+os.sep+str(i))
-    i = i + 1
-    break
